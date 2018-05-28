29df62099b8a5cafbaed8adb81a2e9b6410c51d7
Jane Asawa <asawajane@gmail.com>
2018-05-27 18:05:06 -0700

    changed apiRoutes and htmlRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,40 @@
 
-// should have a GET route with url /api/friends  This will be used to display JSON of all possible friends
+// should have a 
 
-// should have a POST route with url /api/friends  This will be used to handle incoming survey results and compatibility logic
+// should have a 
+// ===============================================================================
+
+// LOAD DATA  ------------------
+// It is linking the routing to a "data" source which holds an array of information on friends (ie. the housemates)
+
+var friends = require("../data/friends");
+
+// ROUTING  ----------------------
+
+module.exports = function(app) {
+    //GET route with url /api/friends  This will be used to display JSON of all possible friends
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+ 
+
+    // API POST Request:
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out the survey... this data is then sent to the server...
+    // Then the server saves the data to the friends array)
+    // 
+    // POST route with url /api/friends  This will be used to handle incoming survey results
+    // and compatibility logic
+
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+
+
+        console.log(friends);
+    });
+
+
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,22 @@
 
-// should have GET route to /survey which should display survey page
+// DEPENDENCIES  -------------------
+// use npm path so when we use path.join the __dirname will join the absolute path to our path string 
+var path = require("path");
 
-// should have a default, catch-all route that leads to home.html which displays the home page
+// ROUTING  -------------------------
+
+module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    // If no matching route is found, then below is a default, catch-all that leads to home.html which displays the home page
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
+
